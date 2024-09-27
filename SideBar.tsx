ac89ec484ff4002581986f1652ae9400b72ebef1
hashim <hashimkakkaden@gmail.com>
2024-09-27 09:10:44 +0530

    checking

---
+++ SideBar.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Menu from './Frontend/src/components/Menu'
+
+
+export const SideBar:React.FC = ()=>{
+    return<>
+    <h1>sideBar</h1>
+    <Menu />
+    </>
+}
