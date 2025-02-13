c686f82ded53e2412a6870b3c62ae491860b8afd
hashim <hashimkakkaden@gmail.com>
2025-02-13 12:35:40 +0530

    updates

--- communication-service/src/index.ts
+++ communication-service/src/index.ts
@@ -11,7 +11,7 @@ const startServer = async (): Promise<void> => {
     const port = process.env.PORT || 2000;
     const app = createServer();   
     console.log("updated" );
-    
+       
     app?.listen(port, () =>
       console.log(`communication-service successfully running on port ${port}`)
     );
