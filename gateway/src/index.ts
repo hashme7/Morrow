b9dd98d2e9a2e6cadeb23004dc44b4328c33c16d
hashim <hashimkakkaden@gmail.com>
2025-02-16 16:02:42 +0530

    updates

--- gateway/src/index.ts
+++ gateway/src/index.ts
@@ -12,6 +12,7 @@ dotenv.config({ path: path.resolve(__dirname, "../.env") });
 const app = express();
 
 app.set("trust proxy", 1);
+console.log("truestedkskak..................................")
 
 const corsOptions = {
   origin: [
