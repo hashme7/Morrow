2a30c3076f6aeee608d49173de1ae299dfdb2cb2
hashim <hashimkakkaden@gmail.com>
2025-02-17 11:46:31 +0530

    updates

--- user-service/dist/infrastructure/config/app.js
+++ user-service/dist/infrastructure/config/app.js
@@ -14,22 +14,13 @@ dotenv_1.default.config({ path: path_1.default.resolve(__dirname, "../../../.env
 const createServer = () => {
     try {
         const app = (0, express_1.default)();
-        app.use((0, morgan_1.default)('tiny'));
+        app.use((0, morgan_1.default)("tiny"));
         app.use(express_1.default.json({ limit: "50mb" }));
         app.use(express_1.default.urlencoded({ extended: true, limit: "50mb" }));
-        const corsOptions = {
-            origin: ["http://localhost:5173", "https://morrow-frontend.vercel.app"],
+        app.use((0, cors_1.default)({
+            origin: "https://morrow.hashim-dev007.online",
             credentials: true,
-            methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-            allowedHeaders: [
-                "Content-Type",
-                "Authorization",
-                "X-Requested-With",
-                "Accept",
-            ],
-        };
-        app.use((0, cors_1.default)(corsOptions));
-        app.options("*", (0, cors_1.default)(corsOptions));
+        }));
         app.use("/", userRoutes_1.default);
         return app;
     }
