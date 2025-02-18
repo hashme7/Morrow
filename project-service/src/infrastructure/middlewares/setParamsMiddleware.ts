92ad591894a05a16509a976a5b7a22ea4d2f3fa9
hashim <hashimkakkaden@gmail.com>
2025-02-18 09:45:03 +0530

    user-service updates

--- project-service/src/infrastructure/middlewares/setParamsMiddleware.ts
+++ project-service/src/infrastructure/middlewares/setParamsMiddleware.ts
@@ -7,6 +7,12 @@ export const modify = (req:Request, res:Response, next:NextFunction) => {
           req.headers.cookie.split("; ").map((c) => c.split("="))
         )
         : {};
+    console.log(`
+        +++++++++++++++++++++++
+        cookies:${cookies}
+        
+        +++++++++++++++++++++++
+        `)
     const decodedAt = JWTService.verifyToken(cookies.accessToken);
     console.log(`
 
