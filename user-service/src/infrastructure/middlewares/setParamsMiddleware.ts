0dcb5ce92a3f1d1ddad12edfa836a6ac861e050f
hashim <hashimkakkaden@gmail.com>
2025-02-22 10:05:53 +0530

    update to latest morrow

--- user-service/src/infrastructure/middlewares/setParamsMiddleware.ts
+++ user-service/src/infrastructure/middlewares/setParamsMiddleware.ts
@@ -2,12 +2,24 @@ import { NextFunction, Request, Response } from "express";
 import { JWTService } from "morrow-common";
 
 export const modify = (req: Request, res: Response, next: NextFunction) => {
-    const cookies = req.headers.cookie
+  const cookies = req.headers.cookie
     ? Object.fromEntries(
         req.headers.cookie.split("; ").map((c) => c.split("="))
       )
     : {};
+  console.log(`
+        +++++++++++++++++++++++
+        cookies:${cookies}
+        
+        +++++++++++++++++++++++
+        `);
   const decodedAt = JWTService.verifyToken(cookies.accessToken);
+  console.log(`
+
+        .................................
+        decodedAt:- ${decodedAt}
+        .................................
+        `);
   if (decodedAt) {
     req.params.userId = decodedAt.id;
   }
